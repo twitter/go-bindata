7986746a066f5ac303d8044c33c6f1cb15683cee
Kenneth, Cho Kai Hung <107766043+KennethCho-CKH@users.noreply.github.com>
2023-03-31 21:25:24 +0100

    Improve Documentations

--- asset.go
+++ asset.go
@@ -2,11 +2,22 @@
 // license. Its contents can be found at:
 // http://creativecommons.org/publicdomain/zero/1.0/
 
+/*
+Package bindata provides a tool for embedding binary data into a Go executable.
+
+Usage:
+
+	import "github.com/jteeuwen/go-bindata"
+
+	var asset bindata.Asset
+	// Use asset.Name and asset.Func to access the asset data.
+
+*/
 package bindata
 
 // Asset holds information about a single asset to be processed.
 type Asset struct {
-	Path string // Full file path.
-	Name string // Key used in TOC -- name by which asset is referenced.
-	Func string // Function name for the procedure returning the asset contents.
+	FilePath string // Full file path of the asset.
+	Name     string // Key used in the table of contents to reference the asset.
+	Func     string // Function name that returns the asset contents.
 }
